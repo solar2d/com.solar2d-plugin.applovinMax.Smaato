ed05e8d288926b92fd9d116b22c19e7930cfe823
Scott Harrison <scottrules44@gmail.com>
2022-02-11 20:45:41 -0500

    Initial Commit

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -6,8 +6,8 @@ local metadata =
 
 		-- This is the name without the 'lib' prefix.
 		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
-		staticLibs = { "c++", "sqlite3", "z", "iconv", "SmaatoSDKInAppBidding", "SmaatoSDKBanner", "SmaatoSDKNative", "SmaatoSDKCore", "SmaatoSDKOpenMeasurement", "SmaatoSDKRichMedia", "SmaatoSDKUnifiedBidding"},
-		frameworks = {"OMSDK_Smaato", "AppLovinMediationSmaatoAdapter"},
+		staticLibs = { "c++", "sqlite3", "z", "iconv"},
+		frameworks = {"OMSDK_Smaato", "AppLovinMediationSmaatoAdapter", "SmaatoSDKInAppBidding", "SmaatoSDKBanner", "SmaatoSDKNative", "SmaatoSDKCore", "SmaatoSDKOpenMeasurement", "SmaatoSDKRichMedia", "SmaatoSDKUnifiedBidding"},
 		frameworksOptional = {"AppTrackingTransparency"},
 		usesSwift = true,
 	},
